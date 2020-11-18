85160b639d6901939b4e525773c9087c195f9514
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-11-18 09:59:08 +0700

    TL_ Thiet ke giao dien form muon sach

---
+++ QLTV/GUI/MuonSach.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QLTV.BLL;
+using QLTV.DTO;
+
+namespace QLTV.Use_Control
+{
+    public partial class MuonSach : UserControl
+    {
+        
+    }
+}
