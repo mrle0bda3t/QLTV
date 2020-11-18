a80d6fbb6f2e55487e42ccb96e55f36e6b434171
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-11-18 10:52:10 +0700

    TQ_DangNhap_GiaoDienTrangDangNhap

---
+++ QLTV/DangNhap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QLTV.DTO;
+using QLTV.BLL;
+
+namespace QLTV
+{
+    public partial class DangNhap : DevExpress.XtraEditors.XtraForm
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
