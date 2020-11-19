607ea0d4ed38903bd0cde0b2a34b7048fde5b70e
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-11-19 07:19:20 +0700

    TQ_TK_Tao giao dien tim kiem sach

---
+++ QLTV/GUI/tkSach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QLTV.BLL;
+using QLTV.DTO;
+
+namespace QLTV.GUI
+{
+    public partial class tkSach : UserControl
+    {
+        
+        public tkSach()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
