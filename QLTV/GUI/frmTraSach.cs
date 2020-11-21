7d7aa62394f2ad1c85c49a14201de8c288be166c
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-11-21 09:20:12 +0700

    TQ_BC_Sua du lieu the loai va tao hoa don tra sach

---
+++ QLTV/GUI/frmTraSach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QLTV.BLL;
+using QLTV.DTO;
+using Microsoft.Reporting.WinForms;
+
+namespace QLTV.Reports
+{
+    public partial class frmTraSach : UserControl
+    {
+        TraSach_BLL tsBUS = new TraSach_BLL();
+
+        public frmTraSach()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
