8b77133e244ea90058b6c6df26bbf41f89778978
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-11-22 10:52:31 +0700

    TL_ Thiet ke form luot muon

---
+++ QLTV/GUI/ttLuotMuon.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace QLTV.GUI
+{
+    partial class ttLuotMuon
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDanhSach = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDanhSach
+            // 
+            this.dgvDanhSach.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDanhSach.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDanhSach.Location = new System.Drawing.Point(0, 0);
+            this.dgvDanhSach.MainView = this.gridView1;
+            this.dgvDanhSach.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDanhSach.Name = "dgvDanhSach";
+            this.dgvDanhSach.Size = new System.Drawing.Size(1147, 554);
+            this.dgvDanhSach.TabIndex = 0;
+            this.dgvDanhSach.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.DetailHeight = 431;
+            this.gridView1.GridControl = this.dgvDanhSach;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.Editable = false;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // ttLuotMuon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvDanhSach);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ttLuotMuon";
+            this.Size = new System.Drawing.Size(1147, 554);
+            this.Load += new System.EventHandler(this.ttLuotMuon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl dgvDanhSach;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+    }
+}
