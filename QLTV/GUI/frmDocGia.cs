e40641a2b5529053c64c2a872f8be44802f040ea
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-11-22 10:44:52 +0700

    TQ_BC_Tao giao dien bao cao doc gia

---
+++ QLTV/GUI/frmDocGia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QLTV.BLL;
+using QLTV.DTO;
+using Microsoft.Reporting.WinForms;
+
+namespace QLTV.Reports
+{
+    public partial class frmDocGia : UserControl
+    {
+        DocGia_BLL dgBUS = new DocGia_BLL();
+
+        public frmDocGia()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
