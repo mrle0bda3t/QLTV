2ce539bb37175a2211c486cbfdfeb57ff2824474
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-11-23 22:09:47 +0700

    BT_ Sua lai su kien click cho phim va chi cho nhap vao so tai o text box

--- QLTV/GUI/cnSach.cs
+++ QLTV/GUI/cnSach.cs
@@ -159,6 +159,20 @@ namespace QLTV.Use_Control
             }
         }
 
+        private void txtGia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
 
+        private void txtSoLuong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
